46c1436ef410a5363964c9479df79fa20ec41577
radiohegel <roger.e.clayton@gmail.com>
2017-01-09 13:29:55 -0700

    completes and manipulates example nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,15 +1,24 @@
 grocery_store_inventory = {
 	meats: {
-		red_meat: "beef"
+		red_meat: "beef",
 		poultry: ["chicken", "turkey"] 
 	},
 	veggies: ["tomato", "onion", "carrot"],
 	fruit: ["apple", "orange"],
 	dessert: {
-		icecream: ["chocolate", "strawberry"]
+		icecream: ["chocolate", "strawberry"],
 		cake: ["carrot"]
 	}
 
 
 }
-puts grocery_store_inventory
+puts grocery_store_inventory[:meats][:poultry][1]
+#returns "turkey"
+
+grocery_store_inventory[:dessert][:cake].push("red velvet")
+puts grocery_store_inventory[:dessert][:cake]
+#returns "carrot", "red velvet"
+
+grocery_store_inventory[:dessert][:cake][1] = ["chocolate frosting", "vanilla frosting"]
+puts grocery_store_inventory[:dessert][:cake][1][1]
+#returns "vanilla frosting"
