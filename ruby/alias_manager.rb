2d2f8fd86451c417669ec4ae6d9ff0ae78e4520b
radiohegel <roger.e.clayton@gmail.com>
2017-01-09 15:18:46 -0700

    revises program structure, utilizing methods for next_vowel and next_consonant

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,25 +4,54 @@
 #Move through name and determine if each letter is a vowel or consonant.
 #Pull index value for each letter from its respective index, and add 1.
 
-def letter_adjuster (letter)
-	if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
-		puts "no"
-	else
-		puts "yes"
-	end
-end
 
-def name_jumbler (inputted_name)
-	reversed_order = inputted_name.split(' ').reverse
-	characters = reversed_order[0].chars
-	characters += reversed_order[1].chars
-	characters.each do |characters|
-		letter_adjuster(characters)
-	end
+
+
+	
+def next_vowel(letter)
+	vowel = "aeiou".index(letter)
+	vowel_adjustment = vowel + 1
+	letter = "aeiou"[vowel_adjustment]
 	
 end
 
-name_jumbler ("roger clayton")
+def next_consonant(letter)
+	consonant = "bcdfghjklmnpqrstvwxyz".index(letter)
+	consonant_adjustment = consonant + 1
+	letter = "bcdfghjklmnpqrstvwxyz"[consonant_adjustment]
+end
 
 
+puts "Please enter a name to code:"
+inputted_name = gets.chomp.split(' ').reverse 
+first_name = inputted_name[0].chars
+last_name = inputted_name[1].chars
+coded_first_name =[]
+coded_last_name =[]
+first_name.each do |letter|
+	if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+		letter = next_vowel(letter)
+	else 
+		letter = next_consonant(letter)
+	end
+	coded_first_name << letter
+end
+last_name.each do |letter|
+	if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+		letter = next_vowel(letter)
+	else 
+		letter = next_consonant(letter)
+	end
+	coded_last_name << letter
+end
 
+
+	puts coded_first_name
+	puts coded_last_name
+
+#reversed_order = inputted_name.split(' ').reverse
+#characters = reversed_order[0].chars
+#characters += reversed_order[1].chars
+#characters.each do |characters|
+#	next_vowel(characters)	
+#end
