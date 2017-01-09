85c84614c2ee77dc9d22aaa933b116ba7be9c828
radiohegel <roger.e.clayton@gmail.com>
2017-01-09 13:41:40 -0700

    init commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,5 @@
+#take name input and separate first and last name into array
+#reverse array
+#Make strings of vowels and consonants to use as indices. 
+#Move through name and determine if each letter is a vowel or consonant.
+#Pull index value for each letter from its respective index, and add 1.
