29d7fc965dc9a3eeda56310d15ed13e5edd19e1c
radiohegel <roger.e.clayton@gmail.com>
2017-01-09 14:10:03 -0700

    begins building letter_adjuster and name_jumbler methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,3 +3,26 @@
 #Make strings of vowels and consonants to use as indices. 
 #Move through name and determine if each letter is a vowel or consonant.
 #Pull index value for each letter from its respective index, and add 1.
+
+def letter_adjuster (letter)
+	if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+		puts "no"
+	else
+		puts "yes"
+	end
+end
+
+def name_jumbler (inputted_name)
+	reversed_order = inputted_name.split(' ').reverse
+	characters = reversed_order[0].chars
+	characters += reversed_order[1].chars
+	characters.each do |characters|
+		letter_adjuster(characters)
+	end
+	
+end
+
+name_jumbler ("roger clayton")
+
+
+
