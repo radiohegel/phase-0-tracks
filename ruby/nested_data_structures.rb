21bc6677da1bf1adf037bc5515cadb94b59bf374
radiohegel <roger.e.clayton@gmail.com>
2017-01-09 13:08:25 -0700

    initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,15 @@
+grocery_store_inventory = {
+	meats: {
+		red_meat: "beef"
+		poultry: ["chicken", "turkey"] 
+	},
+	veggies: ["tomato", "onion", "carrot"],
+	fruit: ["apple", "orange"],
+	dessert: {
+		icecream: ["chocolate", "strawberry"]
+		cake: ["carrot"]
+	}
+
+
+}
+puts grocery_store_inventory
