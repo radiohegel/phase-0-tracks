c5b73d686efe124f6a7eee0c0eca8aa6b6692717
radiohegel <roger.e.clayton@gmail.com>
2017-01-17 13:38:11 -0700

    init commit - defines Santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+	
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		"That was a good #{cookie}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+
+end
