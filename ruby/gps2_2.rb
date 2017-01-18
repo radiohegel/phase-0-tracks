0d04bc1fcda075b203ece7330d7d50caa057d90c
radiohegel <roger.e.clayton@gmail.com>
2017-01-18 16:51:32 -0700

    adds methods for grocery list functions

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -11,29 +11,69 @@ def create_list(items)
 	list_array.each do |items|
 		grocery_hash[items] = 1
 	end
-	p grocery_hash
-
+	grocery_hash
 end
 
-create_list("carrots apples cereal pizza")
+grocery_list = create_list("carrots apples cereal pizza")
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps: insert item into list; if optional quantity is part of argument, change value from one. 
 # output: updated hash
 
+def add_item!(list, item, qty=1)
+	list[item] = qty
+	list
+end
+
+p add_item!(grocery_list, "peas")
+p add_item!(grocery_list, "oranges", 3)
+
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: list, item to remove
+# steps: delete item from list hash
+# output: updated hash
+
+def remove_item!(list, item)
+	list.delete(item)
+	list
+end
+
+p remove_item!(grocery_list, "cereal")
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: list, item, new qty
+# steps: match item to existing hash key, and reassign value to new qty. 
+# output: updated hash
+
+def update_qty!(list, item, qty)
+	counter = 0
+	list.each do |key, value|
+		break if key == item
+		counter += 1
+	end
+	if counter == list.length
+		response = "Sorry, that item isn't in the list"
+		return response
+	else
+		list[item] = qty
+	end
+	list
+end
+
+p update_qty!(grocery_list, "carrots", 3)
+p update_qty!(grocery_list, "beansprouts", 4)
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: grocery list
+# steps: print sentence with data from hash interpolated in. 
+# output: strings with grocery items and qtys.
+
+def print_list(list)
+	list.each do |list, qty|
+		puts "You need #{qty} #{list}"
+	end
+end
+
+print_list(grocery_list)
 
