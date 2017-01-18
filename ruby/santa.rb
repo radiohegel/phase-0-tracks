847d19e8a493582d9cd182a4e78ca5aa0e70664d
radiohegel <roger.e.clayton@gmail.com>
2017-01-18 08:46:53 -0700

    adds basic UI to retrieve attribute inputs for santa initializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,6 +17,28 @@ class Santa
 	end
 
 end
+santas = []
+
+loop do
+	puts "Do you have a santa to register? Please enter 'yes' or 'no':"
+	user_response = gets.chomp
+		if user_response == "yes"
+			puts "Please enter your gender:"
+			santa_gender = gets.chomp
+			puts "Please enter your ethnicity:"
+			santa_ethnicity = gets.chomp
+			santas << Santa.new(santa_gender, santa_ethnicity)
+		elsif user_response == "no"
+			break
+		else
+			puts "Sorry, I didn't catch that."
+		end
+end
+
+p santas
+
+#EXAMPLES:
+#female/black; n/a/Latin@; male/Asian
 
 
 # gregory = Santa.new
