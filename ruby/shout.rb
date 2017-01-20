a8912caaabcd236075106e8ece6a268fbf0ff2b5
radiohegel <roger.e.clayton@gmail.com>
2017-01-20 08:55:09 -0800

    adds yell_angrily and yelling_happily methods to Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,8 @@
 module Shout
-	# methods will go here
+	def self.yell_angrily(words)
+		words + "!!!" + ":("
+	end
+
+	def self.yelling_happily(words)
+		words + "!!!" + ":)" +"It's a bright and sunny day!"
 end
