4f1f98e310a2280471976f5758f00f4c38f1a50b
radiohegel <roger.e.clayton@gmail.com>
2017-01-20 22:00:59 -0800

    changes Shout module to a mixin module, and adds Angry_Driver and Happy_Camper classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,36 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + ":("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + ":)" +"It's a bright and sunny day!"
+# 	end
+# end
+
+# p Shout.yell_angrily("That's it")
+
+# p Shout.yelling_happily("That's it")
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + ":("
+	def yell_angrily(words)
+		words + "!!!" + " :("
 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + ":)" +"It's a bright and sunny day!"
+	def yelling_happily(words)
+		words + "!" + " :)"
 end
+
+class Angry_Driver
+	include Shout
+end
+
+class Happy_Camper
+	include Shout
+end
+
+driver = Angry_Driver.new
+driver.Shout("Get out of my lane")
+
+camper = Happy_Camper.new
+camper.Shout("Today is perfect")
