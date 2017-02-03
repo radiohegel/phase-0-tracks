0ccf6a514cc55d26cfcc82747df5d52301498fe9
radiohegel <roger.e.clayton@gmail.com>
2017-02-03 15:01:39 -0800

    adds psuedocode outline for ruby program, and adds climbers table to database

---
+++ climb_rater/climb_rater.rb
@@ -0,0 +1,25 @@
+#CLIMB-RATER:
+#I have decided to build a program that could be used in a bouldering gym,
+#for allowing climbers to leave and view ratings for different bouldering
+#problems in the gym. My assumption is that each climb will be identified
+#in the gym with a number at its start. Similarly, climbers will each be assigned
+#a membership number, which they can use to leave feedback. 
+
+#METHODS:
+
+#Submit feedback.
+#Add data to reviews table
+
+#Calculate avg. attempts
+
+#Calculate avg. quality rating
+
+#Calculate avg. 
+
+#USER INTERFACE:
+
+#Greet user and ask what climb they need to access
+
+#Prompt user to choose to either leave feedback, or view avgs for the climb.
+
+#Prompt user for their no. of attempts, 1-5 star quality rating, and difficulty grade.
