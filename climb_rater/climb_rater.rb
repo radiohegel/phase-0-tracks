968220541b0c55d74969c922085fa2a1e40625e2
radiohegel <roger.e.clayton@gmail.com>
2017-02-03 17:10:55 -0800

    adds add_review method

--- climb_rater/climb_rater.rb
+++ climb_rater/climb_rater.rb
@@ -5,11 +5,18 @@
 #in the gym with a number at its start. Similarly, climbers will each be assigned
 #a membership number, which they can use to leave feedback. 
 
+require 'sqlite3'
+db = SQLite3::Database.open "climb_rater.db"
 #METHODS:
 
 #Submit feedback.
 #Add data to reviews table
 
+def add_review(db, attempts, rating, grade, climber_id, problem_id)
+	db.execute "INSERT INTO feedback VALUES (?, ?, ?, ?, ?)", [attempts, rating, grade, climber_id, problem_id]
+end
+
+add_review(db, 1, 5, 5, 1, 1)
 #Calculate avg. attempts
 
 #Calculate avg. quality rating
